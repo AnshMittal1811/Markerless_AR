6c889614616307e5324d8860627756d09670f537
AnshMittal1811 <anshm18111996@gmail.com>
2019-09-05 00:09:08 +0530

    first Markerless AR Project

---
+++ Assets/collisionScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisionScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collide)
+    {
+        GameObject explosion = Instantiate(Resources.Load("FlareMobile", typeof(GameObject))) as GameObject;
+        explosion.transform.position = transform.position;
+        Destroy(collide.gameObject);
+        Destroy(explosion, 2);
+
+        if(GameObject.FindGameObjectsWithTag("Player").Length == 0)
+        {
+            GameObject enemy = Instantiate(Resources.Load("enemy", typeof(GameObject))) as GameObject;
+            GameObject enemy1 = Instantiate(Resources.Load("enemy1", typeof(GameObject))) as GameObject;
+            GameObject enemy2 = Instantiate(Resources.Load("enemy2", typeof(GameObject))) as GameObject;
+            GameObject enemy3 = Instantiate(Resources.Load("enemy3", typeof(GameObject))) as GameObject;
+
+        }
+
+        Destroy(gameObject);
+    }
+}
